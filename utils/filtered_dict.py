dfbd1b6426f3beb658e1fc7eadd75c457820dcfe
Kfir Cohen <kfir969@gmail.com>
2022-01-13 01:11:06 +0200

    Fix bug in FilteredDict

--- utils/filtered_dict.py
+++ utils/filtered_dict.py
@@ -6,11 +6,14 @@ class FilteredDict(dict):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.words_dict = copy.copy(self)
+        self.words_dict = self.copy()
+
+    def copy(self):
+        return copy.copy(self)
 
     def filter(self, regex_pattern):
         regex_ob = re.compile(regex_pattern)
-        for item in self.words_dict.items():
+        for item in self.copy().items():
             filtered_word = regex_ob.fullmatch(item[0])
             if not filtered_word:  # is not None
                 self.pop(item[0])
